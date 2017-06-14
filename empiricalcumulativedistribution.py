dfedd97959ad41de4492b5be69f2e2fd5e7f3df5
bimal maharjan <hakubimal@gmail.com>
2017-06-14 10:52:27 -0400

    plotted ecdf for setosa, versicolor, and virginica

--- empiricalcumulativedistribution.py
+++ empiricalcumulativedistribution.py
@@ -18,32 +18,39 @@ def ecdf(data):
     # y-data for the ECDF: y
     y = np.arange(1, n+1) / float(n)
     return x, y
-    
-def plot_ecdf(x_vers,y_vers):
+
+def plot_ecdf():
+
+	# Get  petal length
+	setosa_petal_length = data.data[np.where(data.target == 0)][:,2]
+	versicolor_petal_length = data.data[np.where(data.target == 1)][:,2]
+	virginica_petal_length = data.data[np.where(data.target == 2)][:,2]
+
+	# Compute ECDF for setosa, versicolor, virginica data:
+	x_set, y_set = ecdf(setosa_petal_length)
+	x_vers, y_vers = ecdf(versicolor_petal_length)
+	x_virg, y_virg = ecdf(virginica_petal_length)
+
 	# Generate plot
-	plt.plot(x_vers,y_vers, marker='.',linestyle='none')
+	# Plot all ECDFs on the same plot
+	_ = plt.plot(x_set, y_set, marker ='.', linestyle ='none')
+	_ = plt.plot(x_vers, y_vers, marker ='.', linestyle ='none')
+	_ = plt.plot(x_virg, y_virg, marker ='.', linestyle ='none')	
+	
 	# Make the margins nice
 	_ = plt.margins(0.02)
 
-
-	# Label the axes
-	_ = plt.xlabel('versicolor_petal_length')
+	# Annotate the plot
+	plt.legend(('setosa', 'versicolor', 'virginica'), loc='lower right')
+	_ = plt.xlabel('petal length (cm)')
 	_ = plt.ylabel('ECDF')
-
 	# Display the plot
 	plt.show()
 
 if __name__ == "__main__":
 	
-	# Get versicolor petal length
-	versicolor_petal_length = data.data[np.where(data.target == 1)][:,2]
-	print versicolor_petal_length
-
-	# Compute ECDF for versicolor data: x_vers, y_vers
-	x_vers, y_vers = ecdf(versicolor_petal_length)
-
 	#plot ECDF
-	plot_ecdf(x_vers,y_vers)
+	plot_ecdf()
 	
 
 
