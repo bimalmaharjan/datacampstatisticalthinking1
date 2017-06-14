d924d1aba45c964f4b24c199ad4cbb5b5e8f6c0a
bimal maharjan <hakubimal@gmail.com>
2017-06-14 12:19:53 -0400

    added pearson coefficient

--- covariance.py
+++ covariance.py
@@ -25,3 +25,23 @@ petal_cov = covariance_matrix[0,1]
 
 print(petal_cov)
 
+def pearson_r(x, y):
+    """Compute Pearson correlation coefficient between two arrays."""
+    # Compute correlation matrix: corr_mat
+    
+    corr_mat = np.corrcoef(x,y)
+
+    print corr_mat
+
+
+    # Return entry [0,1]
+    return corr_mat[0,1]
+
+# Compute Pearson correlation coefficient for I. versicolor: r
+
+r = pearson_r(versicolor_petal_length, versicolor_petal_width)
+
+# Print the result
+
+print (r)
+
