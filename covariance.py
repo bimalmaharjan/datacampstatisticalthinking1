c5cd67b5abf9f81f91ccc4631fbae97fb5f4496d
bimal maharjan <hakubimal@gmail.com>
2017-06-14 12:16:46 -0400

    added covariance

---
+++ covariance.py
@@ -0,0 +1,27 @@
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+import numpy as np
+data = load_iris()
+
+versicolor_petal_length = data.data[np.where(data.target == 1)][:,2]
+versicolor_petal_width = data.data[np.where(data.target == 1)][:,3]
+
+# Compute the covariance matrix: covariance_matrix
+
+covariance_matrix = np.cov(versicolor_petal_length, versicolor_petal_width)
+
+
+# Print covariance matrix
+
+print ( covariance_matrix)
+
+
+# Extract covariance of length and width of petals: petal_cov
+
+petal_cov = covariance_matrix[0,1]
+
+
+# Print the length/width covariance
+
+print(petal_cov)
+
