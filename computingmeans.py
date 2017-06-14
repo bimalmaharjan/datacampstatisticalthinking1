a51dcda9b923749d593278845392de54ad058fb0
bimal maharjan <hakubimal@gmail.com>
2017-06-14 11:28:39 -0400

    comparing percentiles with ecdf

--- computingmeans.py
+++ computingmeans.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import empiricalcumulativedistribution as ecdf
 data = load_iris()
 
 versicolor_petal_length = data.data[np.where(data.target == 1)][:,2]
@@ -20,5 +21,21 @@ percentiles = np.array([2.5,25,50,75,97.5])
 ptiles_vers= np.percentile(versicolor_petal_length, percentiles)
 
 # Print the result
-
 print (ptiles_vers)
+
+x_vers,y_vers = ecdf.ecdf(versicolor_petal_length)
+
+# Plot the ECDF
+_ = plt.plot(x_vers, y_vers, '.')
+plt.margins(0.02)
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('ECDF')
+
+# Overlay percentiles as red diamonds.
+_ = plt.plot(ptiles_vers, percentiles/100, marker ='D', color='red',
+         linestyle='none')
+
+# Show the plot
+
+plt.show()
+
