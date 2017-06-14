a7899e97bd28b7766c17fe8107e57d691591d933
bimal maharjan <hakubimal@gmail.com>
2017-06-14 11:22:32 -0400

    adding percentiles

--- computingmeans.py
+++ computingmeans.py
@@ -11,3 +11,14 @@ mean_length_vers = np.mean(versicolor_petal_length)
 
 # Print the result with some nice formatting
 print('I. versicolor:', mean_length_vers, 'cm')
+
+# Specify array of percentiles: percentiles
+percentiles = np.array([2.5,25,50,75,97.5])
+
+
+# Compute percentiles: ptiles_vers
+ptiles_vers= np.percentile(versicolor_petal_length, percentiles)
+
+# Print the result
+
+print (ptiles_vers)
